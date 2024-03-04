1093a687f05538a5c6e3ce42a01d01ff2b56f773
Saim Shujaat Khan <saimshujaatkhan@gmail.com>
2024-03-05 03:09:22 +0500

    BetterSignupForm code Refactored

--- src/BetterSignupForm.component.jsx
+++ src/BetterSignupForm.component.jsx
@@ -7,12 +7,17 @@ function BetterSignupForm() {
   });
 
   const handleChange = (evt) => {
-    const changedField = evt.target.name;
+    const changedField = evt.target.name; // which feild changed? name of that feild.
     const newValue = evt.target.value;
 
     setFormData((currData) => { // take whatever's current data
-      currData[changedField] = newValue; //update the field, whatever the correct field is wit the new value and then return a copy of the object
-      return { ...currData }; //return the copy of that object
+      // currData[changedField] = newValue; //update the field, whatever the correct field is wit the new value and then return a copy of the object
+      // return { ...currData }; //return the copy of that object
+      return {
+        ...currData,
+        [changedField]: newValue,
+        // [evt.target.name] = [evt.target.value], //some people don't create respective varible instead they only write this exptression
+      };
     });
   };
 
