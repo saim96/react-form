97612697d5466fecb8e0b5e7523e923df780b209
Saim Shujaat Khan <saimshujaatkhan@gmail.com>
2024-03-06 04:56:25 +0500

    Form Validation Applied in FormDemo using React Hook Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-// import UsernameForm from "./UsernameForm";
-// import SignupForm from "./SignupForm";
-// import BettersignupForm from "./BetterSignupForm.component";
+import UsernameForm from "./UsernameForm";
+import SignupForm from "./SignupForm";
+import BettersignupForm from "./BetterSignupForm.component";
 import ShoppingList from "./ShoppingList.component";
+import FormDemo from "./FormDemo";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <FormDemo/>
       <ShoppingList />
-      
+      <BettersignupForm/>
+      <SignupForm/>
+      <UsernameForm/>
     </>
   );
 }
